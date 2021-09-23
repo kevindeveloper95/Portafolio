1dc6159905247f48b865509a3fc85d23deb872be
kevin meneses parga <kevindeveloper95@hotmail.com>
2021-09-23 18:50:02 -0500

    portafolio casi terminado solo falta agregar mas proyectos y sus codigos de github

---
+++ email.php
@@ -0,0 +1,60 @@
+<?php
+
+
+/* if(isset($_POST['enviar'])){
+    if(!empty($_POST['name']) &&  !empty($_POST['email']) && !empty($_POST['asunto']) && !empty($_POST['mensaje'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $asunto = $_POST['asunto'];
+        $mensaje = $_POST['mensaje'];
+        $header = "From: kemen95@hotmail.com" . "\r\n";
+        $header .= "Reply-To: kemen95@hotmail.com" . "\r\n";
+        $header.= "X-Mailer: PHP/". phpversion();
+        $email = @mail($email, $asunto,$mensaje,$header);
+        if ($email) {
+           echo "<h4>el mensaje se envio correctamente  </h4>";
+        }
+    }
+} */
+
+
+$destinatario = 'kevindeveloper95@hotmail.com';
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$mensaje = $_POST['message'];
+
+
+$header = "enviado desde el portafolio de Kevin";
+$mensajecompleto = $mensaje . "\nAtentamente:" .  $name;
+
+mail($destinatario, $mensaje,$mensajecompleto,$header);
+
+
+
+echo "<script>
+const alerta = document.querySelector('#alert')
+alerta.addEventListener('submit', aplicar)
+
+
+
+function aplicar(){
+    Swal.fire(
+        'Mensaje enviado!',
+        'You clicked the button!',
+        'success'
+        
+      )
+      setTimeout(function(){
+        window.location.href = 'index.php'; },4000);
+   
+     
+
+}
+ 
+
+  </script>";
+  
+  
+
+
---
+++ index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE HTML>
+<!--
+	Strata by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Portafolio</title>
+		<link rel="shortcut icon" href="./images/briefcase-solid.svg">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!-- Estilos de boton cambio de idioma -->
+		<link rel="stylesheet" href="assets/css/idiomas.css" />
+		<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+		
+	
+	
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="#" class="image avatar"><img src="images/kevin.jpg" alt="" /></a>
+					<div>
+					<span class="es">Español</span>
+					<input type="checkbox"  class="check">
+					<span class="en">Ingles</span>
+					</div>
+					<h1>Hola! soy Kevin Meneses Parga</h1>
+					<h1><strong>DESAROLLADOR FRONTEND</strong></h1>
+					<ul  id="button-change">
+						<li><a  href="#portafolio" class="button portfolio"><i class="fas fa-arrow-circle-right"></i> Ver Portafolio</a></li>
+					</ul>
+					<ul  id="button-change">
+						<li><a  href="images/Kevin-CV-es.pdf" download="" class="button portfolio"><i class="fas fa-download"></i> Descargar CV</a></li>
+					</ul>
+				</div>
+			</header>
+
+			
+
+		<!-- Main -->
+			<div id="main">
+
+				<!-- One -->
+					<section id="one">
+						<header class="major">
+							<h2><i class="fas fa-user-tie"></i> Sobre mi</h2>
+						</header>
+						<p>Soy un chico autodidacta que encontro la pasion que buscaba en el mundo de la tecnologia buscando esa oportunidad de crecer y convertirse en un valioso profesional, aportando trabajos,nuevas ideas y manteniendose constantemente aprendiendo para ser mejor profesional dia con dia</p>
+					</section>
+
+					<section id="one">
+						<header class="major">
+							<h2><i class="fas fa-graduation-cap"></i> Educacion</h2>
+						</header>
+						<p>Licenciatura en nutricion concluida en el CENTRO UNIVERSITARIO DE LA COSTA (CUC), actualmente estudiando la carrera de ingenieria en sistemas computacionales en la escuela Universidad Tecnológica Latinoamericana en línea(UTEL)</p>
+						<div class="container-img">
+						<img class="img-one" src="./images/utel.png" alt="">
+						<img class="img-two" src="./images/cuc.png" alt="">
+					</div>
+					</section>
+
+
+				<!-- Two -->
+					<section id="two">
+						<h2 id="portafolio"><i class="fas fa-briefcase"></i> Portafolio(proyectos destacados)</h2>
+						<div class="row">
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="https://mercado-libre.kevincommerce.com/" class="image fit thumb"><img src="images/mercado-libre.png" alt="" /></a>
+								<h3>Mercado libre</h3>
+								<p>Copia visual de la aplicacion web ecommerce de la tienda popular en linea llamada mercado libre creado con <strong>HTML,</strong> <strong>CSS</strong> y <strong>ANGULAR</strong></p>
+								<div class="button-proyect">
+								<ul  id="button-change">
+						         <li><a  href="https://mercado-libre.kevincommerce.com/" class="button portfolio"><i class="fas fa-arrow-circle-right"></i> Ver proyecto</a></li>
+					           </ul>
+								<ul  id="button-change">
+						         <li><a  href="https://github.com/kevindeveloper95/angular-mercado-libre" class="button portfolio"><i class="icon brands fa-github"></i> Ver Codigo</a></li>
+					           </ul>
+							   </div>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="https://www.healthline.com/health/es/dieta-cetogenica" class="image fit thumb"><img src="images/nutricion.png" alt="" /></a>
+								<h3>Nutricion</h3>
+								<p>Sitio web informativo sobre como se debe alimentar en base a la enfermedad u objetivo hecha con <strong>HTML,</strong><strong>CSS, </strong><strong>BOOTSTRAP, </strong><strong>NODE.JS </strong><strong>Y EXPRESS.</strong></p>
+								<div class="button-proyect">
+								<ul  id="button-change">
+						         <li><a  href="#portafolio" class="button portfolio"><i class="fas fa-arrow-circle-right"></i> Ver proyecto</a></li>
+					           </ul>
+								<ul  id="button-change">
+						         <li><a  href="https://github.com/kevindeveloper95/nutrition-site" class="button portfolio"><i class="icon brands fa-github"></i> Ver Codigo</a></li>
+					           </ul>
+							   </div>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="https://barberia.kevincommerce.com/index.html" class="image fit thumb"><img src="./images/barber-shop.png" alt="" /></a>
+								<h3>Barber shop</h3>
+								<p>pagina web enfocada a una barberia con el fin de practica y aprendizaje usando diferentes estilos, realizado con las tecnologias <strong>HTML,</strong> <strong>CSS</strong> <strong>JAVASCRIPT</strong> y <strong>JQUERY</strong></p>
+								<div class="button-proyect">
+								<ul  id="button-change">
+						         <li><a  href="https://barberia.kevincommerce.com/index.html" class="button portfolio"><i class="fas fa-arrow-circle-right"></i> Ver proyecto</a></li>
+					           </ul>
+								<ul  id="button-change">
+						         <li><a  href="https://github.com/kevindeveloper95/Barber-shop" class="button portfolio"><i class="icon brands fa-github"></i> Ver Codigo</a></li>
+					           </ul>
+							   </div>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a  href="images/fulls/04.jpg" class="image fit thumb"><img src="./images/portafolio.png" alt="" /></a>
+								<h3>Portafolio</h3>
+								<p>Portafolio creado en base a una plantilla, modificada y adaptada con funcionalidades realizado con las tecnologias <strong>HTML,</strong> <strong>CSS,</strong> <strong>JAVASCRIPT</strong> y <strong>PHP</strong>  </p>
+								<div class="button-proyect">
+								<ul  id="button-change">
+						         <li><a  href="#portafolio" class="button portfolio"><i class="fas fa-arrow-circle-right"></i> Ver proyecto</a></li>
+					           </ul>
+								<ul  id="button-change">
+						         <li><a  href="#portafolio" class="button portfolio"><i class="icon brands fa-github"></i> Ver Codigo</a></li>
+					           </ul>
+							   </div>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/05.jpg" class="image fit thumb"><img src="images/thumbs/05.jpg" alt="" /></a>
+								<h3>Nunc enim commodo aliquet</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+								<div class="button-proyect">
+								<ul  id="button-change">
+						         <li><a  href="#portafolio" class="button portfolio"><i class="fas fa-arrow-circle-right"></i> Ver proyecto</a></li>
+					           </ul>
+								<ul  id="button-change">
+						         <li><a  href="#portafolio" class="button portfolio"><i class="icon brands fa-github"></i> Ver Codigo</a></li>
+					           </ul>
+							   </div>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/06.jpg" class="image fit thumb"><img src="images/thumbs/06.jpg" alt="" /></a>
+								<h3>Risus ornare lacinia</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+								<div class="button-proyect">
+								<ul  id="button-change">
+						         <li><a  href="#portafolio" class="button portfolio"><i class="fas fa-arrow-circle-right"></i> Ver proyecto</a></li>
+					           </ul>
+								<ul  id="button-change">
+						         <li><a  href="#portafolio" class="button portfolio"><i class="icon brands fa-github"></i> Ver Codigo</a></li>
+					           </ul>
+							   </div>
+							</article>
+						</div>
+						
+					</section>
+
+				<!-- Three -->
+					<section id="three">
+						<h2>¿Creamos algo juntos?</h2>
+						<div class="row">
+							<div class="col-8 col-12-small">
+								<form method="post" action="" id="alert">
+									<div class="row gtr-uniform gtr-50">
+										<div class="col-6 col-12-xsmall"><input type="text" name="name" id="name" placeholder="Name" pattern="[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{4,25}" required /></div>
+										<div class="col-6 col-12-xsmall"><input type="email" name="email" id="email" placeholder="Email" pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" required/></div>
+										<div class="col-12"><textarea name="message" id="message" placeholder="Message" rows="4" required pattern="[A-Za-z0-9]{5,40}"></textarea></div>
+									</div>	
+									<br>
+									<ul class="actions">
+										<li><input   type="submit" value="Send Message" /></li>
+									</ul>
+								</form>
+								<?php
+								include('email.php');
+								?>
+								
+				
+								
+								
+							</div>
+							<div class="col-4 col-12-small">
+								<ul class="labeled-icons">
+								<li>
+									<h3 class="icon solid fa-envelope"><span class="label">Email</span></h3>
+									<p id="hotmail" href="#">kevindeveloper95@hotmail.com</p>
+								</li>
+									<li>
+									<h3 ><i class="fas fa-map-marked-alt"></i><span class="label"></span></h3>
+										Puerto vallarta<br />
+										Jalisco<br />
+									
+									</li>
+									
+								</ul>
+							</div>
+						</div>
+					</section>
+
+				<!-- Four -->
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="https://www.facebook.com/kevin.menesesparga/" class="icon brands fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="https://github.com/kevindeveloper95?tab=repositories" class="icon brands fa-github"><span class="label">Github</span></a></li>
+						<li><a href="https://www.instagram.com/kevinmenesesparga/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="https://www.linkedin.com/in/kevin-meneses-frontend-web-developer/" class="icon brands fa-linkedin"><span class="label">Linkedin</span></a></li>
+						
+					</ul>
+					<ul class="copyright">
+					<li>Ver mas: <a href="https://candidato.computrabajo.com.mx/Candidate/Home">Computrabajo</a></li><li>Ver mas: <a href="https://www.linkedin.com/in/kevin-meneses-frontend-web-developer/">Linkedin</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+		
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+			<script src="assets/js/sweetalert.js"></script>
+			<!-- sweetalert2 -->
+			<script src="assets/js/idiomas.js"></script>
+			
+			
+			
+			
+
+	</body>
+</html>
